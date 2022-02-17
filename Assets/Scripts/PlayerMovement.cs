af72b3d7e5f5b1014bc950b0f41b2a27f98d4983
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2022-02-17 05:24:05 +0000

    Player can move on x axis

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float playerInput = Input.GetAxis("Horizontal");
+        Vector2 pacmanVelocity = new Vector2(playerInput * moveSpeed, rb.velocity.y);
+        rb.velocity = pacmanVelocity;
+    }
+}
