03bd5eac57a9009717fa930f651354dc09d34843
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2022-02-19 19:29:23 +0000

    Buggy ghost follow AI

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    float moveSpeed = 4f;
+    float MaxDist = 10f;
+    float MinDist = 5f;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        //transform.LookAt(player);
+        if(Vector3.Distance(transform.position,player.position) >= MinDist)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+
+        if(Vector3.Distance(transform.position,player.position) <= MaxDist)
+        {
+            Debug.Log("Test");
+        }
+    }
+}
