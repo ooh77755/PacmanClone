96f247affad1750ef6e8f13d01aa6af1116176e5
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2022-02-19 14:07:13 +0000

    Player can flip sprite in 4 directions!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float moveSpeed = 5f;
-
+    Vector2 direction; 
     Rigidbody2D rb;
     
     void Start()
@@ -17,6 +17,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Move();
+        CheckInput();
+        FlipSprite();
+        //transform.localRotation = Quaternion.Euler(0, 0, 90);
     }
 
     private void Move()
@@ -25,5 +28,55 @@ public class PlayerMovement : MonoBehaviour
         float playerInputVertical = Input.GetAxis("Vertical");
         Vector2 pacmanVelocity = new Vector2(playerInputHorizontal * moveSpeed, playerInputVertical * moveSpeed);
         rb.velocity = pacmanVelocity;
+    }
+
+    private void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        if(direction == Vector2.left)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0f);
+        }
+
+        else if(direction == Vector2.right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0f);
+        }
+
+        else if(direction == Vector2.up)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 90f);
+        }
+
+        else if(direction == Vector2.down)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, -90f);
+        }
     }
 }
