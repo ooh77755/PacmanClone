ee89eb62e5a5c91a81b3b5b11a1b827d4bb509d4
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2022-02-19 19:39:29 +0000

    Enemy chases Player (less buggy)

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float withinRange = 5f;
+    [SerializeField] float speed = 5f;
+
+    void Update()
+    {
+        float dist = Vector2.Distance(target.position, transform.position);
+        float step = speed * Time.deltaTime;
+
+        if (dist <= withinRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, step);
+        }
+
+        else
+        {
+            return;
+        }
+    }
+}
